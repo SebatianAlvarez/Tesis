559e245ebf708eb4176022c0ef4344fbb82ae03d
SebatianAlvarez <pitkiller2.2@gmail.com>
2019-10-22 14:13:53 -0500

    segunda version, con navegavilidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RestaurantesComponent } from './components/restaurantes/restaurantes.component';
+import { ListaRestaurantesComponent } from './components/restaurantes/lista-restaurantes/lista-restaurantes.component';
+import { RestauranteComponent } from './components/restaurantes/restaurante/restaurante.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ListaClientesComponent } from './components/clientes/lista-clientes/lista-clientes.component';
+import { ClienteComponent } from './components/clientes/cliente/cliente.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'Restaurantes', component: RestaurantesComponent},
+  {path: 'Restaurante', component: RestauranteComponent},
+  {path: 'ListaRestaurantes', component: ListaRestaurantesComponent},
+  {path: 'Clientes', component: ClientesComponent},
+  {path: 'Cliente', component: ClienteComponent},
+  {path: 'ListaClientes', component: ListaClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
